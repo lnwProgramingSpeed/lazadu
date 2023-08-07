46b69a094592b3c58586e1bc42882ce92c85ba4c
Phawat Phuengdecha <pawat.pooh@gmail.com>
2023-08-07 11:33:32 +0700

    cart detail component, checkout component

--- src/app/model/static.datasource.ts
+++ src/app/model/static.datasource.ts
@@ -5,22 +5,23 @@ import { Observable, from } from 'rxjs';
 @Injectable()
 export class StaticDataSource {
   private products: Product[] = [
-    new Product(1, 'Product 1', 'Category 1', 'Product 1 (Category 1)', 100),
-    new Product(2, 'Product 2', 'Category 1', 'Product 2 (Category 1)', 100),
-    new Product(3, 'Product 3', 'Category 1', 'Product 3 (Category 1)', 100),
-    new Product(4, 'Product 4', 'Category 1', 'Product 4 (Category 1)', 100),
-    new Product(5, 'Product 5', 'Category 1', 'Product 5 (Category 1)', 100),
-    new Product(6, 'Product 6', 'Category 2', 'Product 6 (Category 2)', 100),
-    new Product(7, 'Product 7', 'Category 2', 'Product 7 (Category 2)', 100),
-    new Product(8, 'Product 8', 'Category 2', 'Product 8 (Category 2)', 100),
-    new Product(9, 'Product 9', 'Category 2', 'Product 9 (Category 2)', 100),
-    new Product(10, 'Product 10', 'Category 2', 'Product 10 (Category 2)', 100),
-    new Product(11, 'Product 11', 'Category 3', 'Product 11 (Category 3)', 100),
-    new Product(12, 'Product 12', 'Category 3', 'Product 12 (Category 3)', 100),
-    new Product(13, 'Product 13', 'Category 3', 'Product 13 (Category 3)', 100),
-    new Product(14, 'Product 14', 'Category 3', 'Product 14 (Category 3)', 100),
-    new Product(15, 'Product 15', 'Category 3', 'Product 15 (Category 3)', 100),
-  ];
+    new Product(1, 'Shiny Widget', 'Category 1', 'Description for Shiny Widget', 150),
+    new Product(2, 'Gizmo Deluxe', 'Category 1', 'Description for Gizmo Deluxe', 200),
+    new Product(3, 'Fantastic Gizmo', 'Category 1', 'Description for Fantastic Gizmo', 250),
+    new Product(4, 'Super Widget', 'Category 1', 'Description for Super Widget', 300),
+    new Product(5, 'Mega Gizmo', 'Category 1', 'Description for Mega Gizmo', 350),
+    new Product(6, 'Elegant Gadget', 'Category 2', 'Description for Elegant Gadget', 400),
+    new Product(7, 'Precision Widget', 'Category 2', 'Description for Precision Widget', 450),
+    new Product(8, 'Innovative Gizmo', 'Category 2', 'Description for Innovative Gizmo', 500),
+    new Product(9, 'Glamour Widget', 'Category 2', 'Description for Glamour Widget', 550),
+    new Product(10, 'Turbo Gizmo', 'Category 2', 'Description for Turbo Gizmo', 600),
+    new Product(11, 'Stylish Gadget', 'Category 3', 'Description for Stylish Gadget', 650),
+    new Product(12, 'Premium Widget', 'Category 3', 'Description for Premium Widget', 700),
+    new Product(13, 'Masterful Gizmo', 'Category 3', 'Description for Masterful Gizmo', 750),
+    new Product(14, 'Ultimate Widget', 'Category 3', 'Description for Ultimate Widget', 800),
+    new Product(15, 'Sleek Gadget', 'Category 3', 'Description for Sleek Gadget', 850),
+];
+
   
   getProducts(): Observable<Product[]> {
     return from([this.products]);
--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { ModelModule } from '../model/model.module';
 import { StoreComponent } from './store.component';
 import { CartSummaryComponent } from './cart-summary.component';
+import { CartDetailComponent } from './cart-detail.component';
+import { CheckoutComponent } from './checkout.component';
 
 @NgModule({
   imports: [ModelModule, BrowserModule, FormsModule],
-  declarations: [StoreComponent, CartSummaryComponent],
-  exports: [StoreComponent],
+  declarations: [StoreComponent, CartSummaryComponent, CartDetailComponent, CheckoutComponent,],
+  exports: [StoreComponent, CartDetailComponent, CheckoutComponent],
 })
-export class StoreModule {}
+export class StoreModule { }
